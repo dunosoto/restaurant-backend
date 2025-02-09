920830c88a136a76dbace970a6e58720f004c8b3
Daniel Uno <danielunosoto@gmail.com>
2025-02-09 14:28:36 -0400

    added seeders, categories and products

---
+++ src/seeder/seeder.service.ts
@@ -0,0 +1,45 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { dessertsCategory, drinksCategory, pizzasCategory, soupsCategory } from './data';
+
+@Injectable()
+export class SeederService {
+
+  constructor(
+    private readonly prisma: PrismaService
+  ) {}
+
+  async createCategoriesWithProducts() {
+    try {
+      await this.prisma.product.deleteMany();
+      await this.prisma.category.deleteMany();
+
+      const desserts = this.prisma.category.create({
+        data: dessertsCategory
+      })
+
+      const drinks = this.prisma.category.create({
+        data: drinksCategory
+      })
+
+      const pizzas = this.prisma.category.create({
+        data: pizzasCategory
+      })
+
+      const soups = this.prisma.category.create({
+        data: soupsCategory
+      })
+
+      await Promise.all([ desserts, drinks, pizzas, soups ]);
+
+      return {
+        message: "data inserted correctlly"
+      }
+
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException("review logs into the system.")
+    }
+  }
+
+}
